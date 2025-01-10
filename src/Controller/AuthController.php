978989cadbb0602bfb8d31a4b29ac9083748c243
Le Ngan <le.ngan@lundimatin.fr>
2025-01-10 20:40:27 +0700

    user + security

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class AuthController extends AbstractController
+{
+    #[Route('/auth', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'user' => $user->getUserIdentifier(),
+        ]);
+    }
+}
