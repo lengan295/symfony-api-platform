aca3d0760d83b62ecca199d9ec64b82fd4070f34
Le Ngan <le.ngan@lundimatin.fr>
2025-01-11 11:29:12 +0700

    rate limiter

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,15 +5,23 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class AuthController extends AbstractController
 {
     #[Route('/auth', name: 'api_login', methods: ['POST'])]
-    public function index(#[CurrentUser] ?User $user): JsonResponse
+    public function index(#[CurrentUser] ?User $user, Request $request, RateLimiterFactory $authLimiter): JsonResponse
     {
+        $limiter = $authLimiter->create($request->getClientIp());
+        if (false === $limiter->consume(1)->isAccepted()) {
+            throw new TooManyRequestsHttpException();
+        }
+
         if (null === $user) {
             return $this->json([
                 'message' => 'missing credentials',
